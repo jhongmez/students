f9ce112cdd9adf950c01112a37711432a2e284e1
jhongmez7 <gmezjhon@gmail.com>
2020-10-08 15:12:58 -0500

    Rutas de proyectos tudents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { StudentsComponent } from './components/students/students.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path:'home',      component: HomeComponent},
+	{ path:'about',     component: AboutComponent},
+	{ path:'students',  component: StudentsComponent},
+	{ path:'**',  pathMatch:'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
