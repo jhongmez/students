b9f21d0501d092b281684ceb27ba74567fd6ddf6
jhongmez7 <gmezjhon@gmail.com>
2020-10-07 22:48:33 -0500

    Componentes creados navbar, home, about y students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuNavegacionComponent } from './components/menu-navegacion/menu-navegacion.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavegacionComponent,
+    HomeComponent,
+    AboutComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
